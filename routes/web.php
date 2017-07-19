9089d41761e1be19dd48ca9484e5bb74b0b32b2d
Claudio Acioli <kllaudyo@gmail.com>
2017-07-18 21:58:00 -0300

    Criadas rotas para crUds Conta e Categoria

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,16 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['middleware'=>'auth'], function (){
 
     Route::get('/contas', 'ContaController@index');
-    Route::get('/contas/{id}', 'ContaController@show')->where("id","[0-9]+");
+    Route::get('/contas/{id}', 'ContaController@edit')->where("id","[0-9]+");
     Route::get('/contas/create', 'ContaController@create');
     Route::post('/contas', 'ContaController@store');
+    Route::put('/contas/{id}', 'ContaController@update')->where("id","[0-9]+");
 
     Route::get('/categorias', 'CategoriaController@index');
     Route::get('/categorias/create', 'CategoriaController@create');
+    Route::get('/categorias/{id}', 'CategoriaController@edit')->where("id","[0-9]+");
     Route::post('/categorias', 'CategoriaController@store');
+    Route::put('/categorias/{id}', 'CategoriaController@update')->where("id","[0-9]+");
 
     Route::get('/movimentos/{data?}','MovimentoController@index')->where("data","(0[1-9]|10|11|12)20[0-9]{2}$");
     Route::get('/movimentos/create', 'MovimentoController@create');
