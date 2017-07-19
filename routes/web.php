170dff28d377e221280ac7fc401a6aaef8b63460
Claudio Acioli <kllaudyo@gmail.com>
2017-07-18 23:10:15 -0300

    Criadas rotas para Movimento

--- routes/web.php
+++ routes/web.php
@@ -20,19 +20,21 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::group(['middleware'=>'auth'], function (){
 
     Route::get('/contas', 'ContaController@index');
-    Route::get('/contas/{id}', 'ContaController@edit')->where("id","[0-9]+");
+    Route::get('/contas/{id}', 'ContaController@edit')->where('id','[0-9]+');
     Route::get('/contas/create', 'ContaController@create');
     Route::post('/contas', 'ContaController@store');
-    Route::put('/contas/{id}', 'ContaController@update')->where("id","[0-9]+");
+    Route::put('/contas/{id}', 'ContaController@update')->where('id','[0-9]+');
 
     Route::get('/categorias', 'CategoriaController@index');
     Route::get('/categorias/create', 'CategoriaController@create');
-    Route::get('/categorias/{id}', 'CategoriaController@edit')->where("id","[0-9]+");
+    Route::get('/categorias/{id}', 'CategoriaController@edit')->where('id','[0-9]+');
     Route::post('/categorias', 'CategoriaController@store');
-    Route::put('/categorias/{id}', 'CategoriaController@update')->where("id","[0-9]+");
+    Route::put('/categorias/{id}', 'CategoriaController@update')->where('id','[0-9]+');
 
-    Route::get('/movimentos/{data?}','MovimentoController@index')->where("data","(0[1-9]|10|11|12)20[0-9]{2}$");
+    Route::get('/movimentos/{data?}','MovimentoController@index')->where('data','(0[1-9]|10|11|12)20[0-9]{2}$');
     Route::get('/movimentos/create', 'MovimentoController@create');
+    Route::get('/movimentos/{id}', 'MovimentoController@edit')->where('id','[0-9]+');
     Route::post('/movimentos', 'MovimentoController@store');
+    Route::put('/movimentos/{id}', 'MovimentoController@update')->where('id','[0-9]+');
 
 });
