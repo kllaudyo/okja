3dd481f793be1608487a706facb284ffd06eb3eb
Claudio Acioli <kllaudyo@gmail.com>
2017-07-20 22:35:22 -0300

    Implementado diretiva wecash_confirmed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,16 +18,16 @@ class AppServiceProvider extends ServiceProvider
             return "<?php echo ({$expression}?'active':''); ?>";
         });
 
-        Blade::directive('datetime', function ($expression) {
-            return "<?php echo ($expression)->format('m/d/Y H:i'); ?>";
-        });
-
         Blade::directive('html_checked', function($expression){
             return "<?php echo ({$expression}?'checked':''); ?>";
         });
 
         Blade::directive('html_selected', function($expression){
             return "<?php echo ({$expression}?'selected':''); ?>";
+        });
+
+        Blade::directive('wecash_confirmed', function($expression){
+            return "<?php echo ({$expression}?'tr-confirmed':''); ?>";
         });
     }
 
